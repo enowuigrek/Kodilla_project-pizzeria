5cc65d18a0ffafdae9a23608b0f7533327a9c997
Łukasz Nowak <enowuigrek@gmail.com>
2022-04-02 21:49:56 +0200

    add subpages - 9.4

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,66 @@ import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function (){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    };
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        /*run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+
+      });
+    };
+
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+     
+    /* class "active" to matching pages, remove from non-matching */
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /* class "active" to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+
+  },
+
   initMenu: function(){
     const thisApp = this;
 
     for (let productData in thisApp.data.products){
       new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
-    }
+    };
   },
 
   initCart: function(){
@@ -23,6 +77,7 @@ const app = {
       app.cart.add(event.detail.product);
     }); 
   },
+
   initData: function(){
     const thisApp = this;
 
@@ -48,9 +103,11 @@ const app = {
     console.log('settings:', settings);
     console.log('templates:', templates);
 
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
+
 };
 
 app.init();
