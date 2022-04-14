81bdcef3740fa58afc0e0d1c1f53b58760bfab6e
Łukasz Nowak <enowuigrek@gmail.com>
2022-04-14 16:45:49 +0200

    add home page-to fix - 10.3

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,12 +1,11 @@
-import {templates} from '../settings.js';
+import {templates, select, classNames} from '../settings.js';
 
 class Home{
   constructor(wrapper){
     const thisHome = this;
 
     thisHome.render(wrapper);
-    // thisHome.initWidgets();
-    // thisHome.getData();
+    thisHome.initLink();
   }
 
 
@@ -21,11 +20,40 @@ class Home{
     thisHome.dom.wrapper.innerHTML = generatedHTML;
   }
 
-  // initWidgets(){
-  //   const thisHome = this;
+  initLink() {
+    const thisHome = this;
+    thisHome.links = document.querySelectorAll('.link');
 
+    for (let link of thisHome.links) {
+      link.addEventListener('click', function (event) {
+        const clickedLink = this;
+        event.preventDefault();
+        
+        const id = clickedLink.getAttribute('href').replace('#', '');
+ 
+        thisHome.clickedPage(id);
 
-  // }
+        window.location.hash = '#/' + id;
+      });
+    }
+  }
+
+  clickedPage(pageId) {
+    const thisHome = this;
+    thisHome.pages = document.querySelector(select.containerOf.pages).children;
+    thisHome.navLinks = document.querySelectorAll(select.nav.links);
+
+    for (let page of thisHome.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+      for (let link of thisHome.navLinks) {
+        link.classList.toggle(
+          classNames.nav.active,
+          link.getAttribute('href') == '#' + pageId
+        );
+      }
+    }
+  }
+  
 }
 
 export default Home;
